a376adc2576d22a4037a165309e36dbb691d2e90
Brohmarr <panza.thiagoj@gmail.com>
2023-09-29 16:31:59 -0300

    Add logic to ask for the user's name and to end the game + bug fixes + refactoring +  cleanup

--- script.py
+++ script.py
@@ -12,7 +12,7 @@
 # [X] Make the player able to shoot at a specific position in the board;
 # [X] Create the points system;
 # [ ] Create the "Adversary" (AI) game loop;
-# [ ] Make sure the game is finished when the last ship is destroyed.
+# [X] Make sure the game is finished when the last ship is destroyed.
 
 
 import random as rnd
@@ -44,6 +44,7 @@ class AdversaryAI:
   def __init__(self):
     self.name = "General Robson"
     self.score = 0
+    self.last_shot = []
 
 # This class holds the data for the user.
 class Player:
@@ -52,6 +53,7 @@ class Player:
   def __init__(self, name: str):
     self.name = name
     self.score = 0
+    self.last_shot = []
 
 # This class controls the game loop.
 class GameMaster:
@@ -81,11 +83,15 @@ class GameMaster:
           is_ship_placed = True
 
   # Initializing the game master (GM) attributes.
-  def __init__(self, player_name: str):
+  def __init__(self):
     # Creating the players (user + AI).
+    player_name = input("Enter your name, please (limit of 14 characters): ")
+    if len(player_name) > 14:
+      player_name = player_name[:14]
     self.player = Player(player_name)
     self.adversary = AdversaryAI()
     self.turn = "player"
+    self.game_over = False
 
     # Creating the ships.
     self.ships_sizes_by_quantity = {2: 4, 3: 3, 4: 2, 5: 1}
@@ -132,12 +138,24 @@ class GameMaster:
     # Removing the ship from the game.
     self.ships.pop(self.ships.index(ship))
 
+    # Checking if all the ships were destroyed to end the game.
+    if len(self.ships) == 0:
+      self.game_over_message()
+
   # Changes the game board to be the same as the randomly generated hidden
   #     one at the coordinates received.
   def update_board(self, x: int, y: int):
     # Setting the "displayed board" to be equal the randomly generated
     #     one, with the ships.
     self.board[x][y] = self.hidden_board[x][y]
+    
+    # Saving the coordinates of the current player's last successful shot.
+    numbers_to_letters = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h', 9: 'i', 10: 'j'}
+    
+    if self.turn == "player":
+      self.player.last_shot = [numbers_to_letters[y + 1].upper(), x + 1]
+    else:
+      self.adversary.last_shot = [x, y]
 
     # Checking if a ship was hit, to update its status.
     if self.board[x][y] == 'o':
@@ -181,6 +199,17 @@ class GameMaster:
         else:
           print(column + "  ", end = "")
 
+  def format_score(self, score: int) -> str:
+    str_score = str(score)
+    final_str = ""
+
+    counter = 4 - len(str_score)
+    while counter > 0:
+      final_str += '0'
+      counter -= 1
+    
+    return final_str + str_score
+
   # Prints the whole game information to the terminal.
   def display_game_window(self):
     # Adding a blank line just for precaution...
@@ -203,27 +232,41 @@ class GameMaster:
     print("Ships Remaining: ", end = "")
     ships_remaining_per_size = {}
     for ship in self.ships:
-      if (len(ship.coordinates) - 2) in ships_remaining_per_size:
-        ships_remaining_per_size[(len(ship.coordinates) - 2)] += 1
+      if (len(ship.coordinates)) in ships_remaining_per_size:
+        ships_remaining_per_size[(len(ship.coordinates))] += 1
       else:
-        ships_remaining_per_size[(len(ship.coordinates) - 2)] = 1
+        ships_remaining_per_size[(len(ship.coordinates))] = 1
+    
+    # Making sure their entries exists even if there are no more ships.
+    if ships_remaining_per_size.get(2) == None:
+      ships_remaining_per_size[2] = 0
+    if ships_remaining_per_size.get(3) == None:
+      ships_remaining_per_size[3] = 0
+    if ships_remaining_per_size.get(4) == None:
+      ships_remaining_per_size[4] = 0
+    if ships_remaining_per_size.get(5) == None:
+      ships_remaining_per_size[5] = 0
+
     print("{quantity_2}x (O  O) ; {quantity_3}x (O  O  O) ; {quantity_4}x (O  O  O  O) ; {quantity_5}x (O  O  O  O  O)".format(
-      quantity_2 = ships_remaining_per_size[0],
-      quantity_3 = ships_remaining_per_size[1],
-      quantity_4 = ships_remaining_per_size[2],
-      quantity_5 = ships_remaining_per_size[3]
+      quantity_2 = ships_remaining_per_size[2],
+      quantity_3 = ships_remaining_per_size[3],
+      quantity_4 = ships_remaining_per_size[4],
+      quantity_5 = ships_remaining_per_size[5]
     ))
 
     # Score board.
-    print("Score Board:")
-    print("    {player_name}: {player_score}".format(
+    print("Score Board: {player_name} ({player_score}) - {player_last_shot}, {adversary_name} ({adversary_score}) - {adversary_last_shot}".format(
       player_name = self.player.name,
-      player_score = self.player.score
-    ))
-    print("    {adversary_name}: {adversary_score}".format(
+      player_score = self.format_score(self.player.score),
+      player_last_shot = self.player.last_shot,
       adversary_name = self.adversary.name,
-      adversary_score = self.adversary.score
+      adversary_score = self.format_score(self.adversary.score),
+      adversary_last_shot = self.adversary.last_shot
     ))
+    print()
+
+    # Legends.
+    print("Legends: '*' = Untargeted ; '~' = Water ; 'o' = Ship ; 'x' = Destroyed Ship")
     print()
 
   # Checks if the received coordinates are valid or not.
@@ -258,11 +301,14 @@ class GameMaster:
         self.display_player_input_line("These coordinates are not valid. Please, choose another one: ")
         coord_to_shoot = input()
         
+        # Checking if the input was given following the correct instructions:
+        #     [LETTER][SPACE][NUMBER]
         coords = coord_to_shoot.split()
         if len(coords) >= 2 and not coords[1].isdigit():
           coord_to_shoot = ""
           coords = []
 
+      # Checking if the coordinates received are valid.
       if self.check_if_coordinates_are_valid(letters_to_numbers, range(1, 11), coords[0].lower(), int(coords[1])):
         # Transforming the letter from the x axis to an integer.
         y = letters_to_numbers[coords[0].lower()]
@@ -270,6 +316,7 @@ class GameMaster:
         # Transforming the "string number" from the y axis to an integer.
         x = int(coords[1])
 
+        # Checking if the coordinates have already been hit.
         if self.check_if_coordinates_are_targetable(x, y):
           return x, y
         
@@ -278,6 +325,7 @@ class GameMaster:
           self.display_player_input_line("These coordinates were already targeted. Please, choose another one: ")
           coord_to_shoot = input()
 
+      # If the coordinates are not valid, ask for input again.
       else:
         self.display_game_window()
         self.display_player_input_line("These coordinates are not valid. Please, choose another one: ")
@@ -298,63 +346,130 @@ class GameMaster:
         x, y = self.convert_input_to_coordinates(new_coords)
 
         continue
+  
+  # Ends the game.
+  def game_over_message(self):
+    # Checking who was the winner (the one with the highest score).
+    winner = Player("None")
+    if self.player.score > self.adversary.score:
+      winner = self.player
+    else:
+      winner = self.adversary
+    
+    # Creating the final message to the player based on the winner of the match.
+    final_message = "Game Over! {winner_name} is the winner with {winner_score} points! Press 'e' to exit: ".format(
+      winner_name = winner.name,
+      winner_score = winner.score
+    )
+    
+    # Asking the player to exit the game.
+    input_to_end_it = ''
+    while input_to_end_it.lower() != 'e':
+      self.display_game_window()
+      self.display_player_input_line(final_message)
+      input_to_end_it = input()
+    
+    # Game Over!
+    self.game_over = True
 
 
 # TESTING PHASE
-def testing():
-  # Is the board setup correct? Yeap!
-  gm = GameMaster("Brohmarr")
+# def testing():
+#   # Is the board setup correct? Yeap!
+#   gm = GameMaster()
 
-  # Is the board being printed correctly with and without the ships? Yeap!
-  print(gm.board)
-  print()
-  print(gm.ships)
-  print()
-  print(gm.hidden_board)
-  print()
+#   # Is the board being printed correctly with and without the ships? Yeap!
+#   print(gm.board)
+#   print()
+#   print(gm.ships)
+#   print()
+#   print(gm.hidden_board)
+#   print()
 
-  # Are the ships being positioned correctly in the board (no overlap)? Yeap!
-  for ship in gm.ships:
-    print(ship.coordinates)
-  print()
-  gm.display_board(gm.board)
-  print()
-  gm.display_board(gm.hidden_board)
-  print()
+#   # Are the ships being positioned correctly in the board (no overlap)? Yeap!
+#   for ship in gm.ships:
+#     print(ship.coordinates)
+#   print()
+#   gm.display_board(gm.board)
+#   print()
+#   gm.display_board(gm.hidden_board)
+#   print()
 
-  # Is the game window being displayed correctly? Yeap!
-  # gm.display_game_window()
-  # gm.ships.pop(0)
-  # gm.display_game_window()
-  # gm.display_player_input_line()
-  # print()
+#   # Is the game window being displayed correctly? Yeap!
+#   # gm.display_game_window()
+#   # gm.ships.pop(0)
+#   # gm.display_game_window()
+#   # gm.display_player_input_line()
+#   # print()
 
-  # Is the shoot function working? Yeap!
-  gm.shoot_at_coordinates(5, 5)
-  gm.display_game_window()
-  gm.display_player_input_line()
+#   # Is the shoot function working? Yeap!
+#   gm.shoot_at_coordinates(5, 5)
+#   gm.display_game_window()
+#   gm.display_player_input_line()
 
-  # Is the player able to shoot? Yeap!
-  gm.shoot_at_coordinates(5, 5)
+#   # Is the player able to shoot? Yeap!
+#   gm.shoot_at_coordinates(5, 5)
 
-  # Is the ship destroyable?
-  while len(gm.ships) == 10:
-    gm.display_game_window()
-    gm.display_player_input_line()
-    target = input()
-    x, y = gm.convert_input_to_coordinates(target)
-    gm.shoot_at_coordinates(x, y)
-  gm.display_game_window()
-  print(gm.ships)
+#   # Is the ship destroyable?
+#   while len(gm.ships) == 10:
+#     gm.display_game_window()
+#     gm.display_player_input_line()
+#     target = input()
+#     x, y = gm.convert_input_to_coordinates(target)
+#     gm.shoot_at_coordinates(x, y)
+#   gm.display_game_window()
+#   print(gm.ships)
+
+# TESTING THE GAME OVER FUNCTION
+# def testing_game_over():
+#   gm = GameMaster()
+  
+#   gm.ships = [gm.ships[0]]
+  
+#   print(gm.ships[0].coordinates)
+
+#   while not gm.game_over:
+#     # Display the game window.
+#     gm.display_game_window()
+#     gm.display_player_input_line()
+    
+#     # Get the player input.
+#     target = input()
+    
+#     # Convert it to coordinates in the game board.
+#     x, y = gm.convert_input_to_coordinates(target)
+    
+#     # Shoot at those coordinates.
+#     gm.shoot_at_coordinates(x, y)
+
 
 # MAIN
 def main():
-  # TODO: Start the game here...
-  pass
+  gm = GameMaster()
+
+  while not gm.game_over:
+    if gm.turn == "player":
+      # Display the game window.
+      gm.display_game_window()
+      gm.display_player_input_line()
+
+      # Get the player input.
+      target = input()
+
+      # Convert it to coordinates in the game board.
+      x, y = gm.convert_input_to_coordinates(target)
+
+      # Shoot at those coordinates.
+      gm.shoot_at_coordinates(x, y)
+    
+    else:
+      # The adversary (AI) make their shot.
+      pass #TODO: Complete this!
 
 
 # Testing the code...
-testing()
+#testing()
+#testing_game_over()
 
 # Starting the game...
-#main()
+main()
